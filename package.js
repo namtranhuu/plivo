9dc2660172486aae92d3a7f080a71fbeec224c28
namtranhuu <namtranhuu@users.noreply.github.com>
2015-02-10 07:51:00 -0800

    package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+    name: 'namthhd:plivo',
+    summary: 'Plivo NodeJS helper library for Meteor',
+    version: '0.0.1',
+    git: 'https://github.com/namtranhuu/plivo'
+});
+
+Npm.depends({
+    "plivo-node": '0.3.0'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('METEOR@1.0.3.1');
+    api.export('Plivo');
+    api.addFiles('lib/plivo.js', 'server');
+});
